39859d19b26611588f1d54529287b971de1c451b
Balázs Dukai <balazs.dukai@gmail.com>
2023-06-13 16:44:24 +0200

    Change license

--- src/cityjson2ifc_cli/convert.py
+++ src/cityjson2ifc_cli/convert.py
@@ -1,18 +1,20 @@
-"""Converter module (convert.py)
-Copyright 2023 3DGI <info@3dgi.nl>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+# cityjson2ifc - Python CityJSON to IFC converter
+# Copyright (C) 2023 3DGI <info@3dgi.nl>
+#
+# This file is part of cityjson2ifc.
+#
+# cityjson2ifc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cityjson2ifc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with cityjson2ifc.  If not, see <http://www.gnu.org/licenses/>.
 
 from ifccityjson.cityjson2ifc import Cityjson2ifc
 
